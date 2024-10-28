16320ed3d6e82d21cb6b9a9fd64c4bf4cfd45199
Choi, Minwoo <chaiminwoo0223@gmail.com>
2024-10-28 16:40:58 +0900

    refactor: 메소드 레퍼런스를 활용해 조금 더 깔끔한 코드로 변경하기

--- library-app/src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
@@ -19,6 +19,9 @@ class UserLoanHistory(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
 ) {
+    val isReturn: Boolean
+        get() = this.status == UserLoanStatus.RETURNED
+
     fun doReturn() {
         this.status = UserLoanStatus.RETURNED
     }
