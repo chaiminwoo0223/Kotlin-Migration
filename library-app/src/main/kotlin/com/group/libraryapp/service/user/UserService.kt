8ff6b041b1073d07979c946b7bb5f1aae304ac49
Choi, Minwoo <chaiminwoo0223@gmail.com>
2024-10-28 16:10:06 +0900

    refactor: N+1 문제를 해결하는 방법 - fetch join

--- library-app/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
+++ library-app/src/main/kotlin/com/group/libraryapp/service/user/UserService.kt
@@ -43,7 +43,7 @@ class UserService(
 
     @Transactional(readOnly = true)
     fun getUserLoanHistories(): List<UserLoanHistoryResponse> {
-        return userRepository.findAll().map { user -> UserLoanHistoryResponse(
+        return userRepository.findAllWithHistories().map { user -> UserLoanHistoryResponse(
             name = user.name,
             books = user.userLoanHistories.map { history -> BookHistoryResponse(
                 name = history.bookName,
